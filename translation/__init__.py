57151b71a03b804e7815b847364fbeeee7cfb65e
Blue <blue@nqn.blue>
2021-02-23 19:43:42 +0000

    Add ms to locales

--- translation/__init__.py
+++ translation/__init__.py
@@ -17,7 +17,8 @@ class Translator:
         "hi": "hi",
         "fr": "fr",
         "pt": "pt",
-        "br": "pt"
+        "br": "pt",
+        "ms": "ms"
     }
 
     flags = {
@@ -28,7 +29,7 @@ class Translator:
     }
 
     flag_emojis = {flag: "".join(chr(0x1f185+ord(c)) for c in flag) for flag in flags}
-    hidden_locales = ["owo", "hi", "fr"]
+    hidden_locales = ["owo", "hi", "fr", "ms"]
 
     def __init__(self, get_locale: Callable[[Guild], Awaitable[str]]):
         self.get_locale = get_locale
